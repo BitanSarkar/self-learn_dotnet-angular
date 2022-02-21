e83dde89149aafe732bf4b0c8aa1fa41a584d785
Bitan Sarkar <bitsarka@publicisgroupe.net>
2022-02-21 15:36:21 +0530

    end of section 4

---
+++ API/DTOs/RegisterDTO.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class RegisterDTO
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(10)]
+        public string UserName {get; set;}
+
+        [Required]
+        [MinLength(8)]
+        [MaxLength(10)]
+        public string Password { get; set; }
+    }
+}
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace API.Entities
@@ -8,7 +9,13 @@ namespace API.Entities
     public class AppUser
     {
         public int Id { get; set; }
+
         public string UserName { get; set; }
-        
+
+        [JsonIgnore]
+        public byte[] PasswordHash {get; set;}
+
+        [JsonIgnore]
+        public byte[] PasswordSalt { get; set; }
     }
 }
